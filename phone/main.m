9761d430449e8d8197f4e2e16844d449fe32031f
NavaneethBhat <navaneeth.y.v@gmail.com>
2013-09-15 23:33:01 +0530

    refined older code messages

--- phone/main.m
+++ phone/main.m
@@ -9,26 +9,26 @@ int main()
 	phonecard *p2=[[phonecard alloc]init];
 	phonecard *p3=[[phonecard alloc]init];
 
-	[p1 setName:@"nagesh"];
-	[p1 setEmail:@"nag94@gmail.com"];
+	[p1 setName:@"mad"];
+	[p1 setEmail:@"mad@gmail.com"];
 	[p1 setNo:1];
 	
-	[p2 setName:@"madan"];
-	[p2 setEmail:@"madan@gmail.com"];
+	[p2 setName:@"nag"];
+	[p2 setEmail:@"nag@gmail.com"];
 	[p2 setNo:2];
 
-	[p3 setName:@"karan"];
-	[p3 setEmail:@"karru@gmail.com"];
+	[p3 setName:@"abhi"];
+	[p3 setEmail:@"abhi@gmail.com"];
 	[p3 setNo:3];
 	
-	phonebook *p_book=[[phonebook alloc]initWithName:@"Abhi's book"];
+	phonebook *p_book=[[phonebook alloc]initWithName:@"Phone book maintained by Navaneeth"];
 	
 	[p_book addobj:p1];
 	[p_book addobj:p2];
 	[p_book addobj:p3];
-	[p_book findByName:@"karan"];
+	[p_book findByName:@"abhi"];
 	[p_book print];
-	[p_book deleteContact:@"nagesh"];
+	[p_book deleteContact:@"nag"];
 	[p_book print];
 	[myPool drain];
 	return 0;
